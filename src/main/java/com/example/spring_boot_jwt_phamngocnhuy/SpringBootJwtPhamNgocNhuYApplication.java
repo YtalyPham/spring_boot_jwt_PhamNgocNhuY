73d4dadc2167ac643737d2594a46bdb02eaa2247
YtalyPham <nhuyphamdev@gmail.com>
2021-09-23 15:54:09 +0700

    Create project

---
+++ src/main/java/com/example/spring_boot_jwt_phamngocnhuy/SpringBootJwtPhamNgocNhuYApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_phamngocnhuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtPhamNgocNhuYApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtPhamNgocNhuYApplication.class, args);
+    }
+
+}
